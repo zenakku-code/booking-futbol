3eed066c329a0818a7adcf8013a5d8376597103b
ezequiel <zamoranoezequiel3@gmail.com>
2026-01-29 11:29:11 -0300

    feat: disable vercel toolbar

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "@libsql/client", "@prisma/adapter-libsql"],
+  devIndicators: {
+    appIsrStatus: false,
+  },
 };
 
 export default nextConfig;
