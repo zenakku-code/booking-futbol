b72336a71e8668ab7e64ae2615aa34ed7ae45252
ezequiel <zamoranoezequiel3@gmail.com>
2026-01-29 02:36:59 -0300

    chore: Add ESLint rules for TypeScript, React hooks, and Next.js image elements.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,12 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "@next/next/no-img-element": "warn"
+    },
     ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
   },
 ];
